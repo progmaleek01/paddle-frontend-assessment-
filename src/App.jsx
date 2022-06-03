ddec3504d9a901cfef07c5a51017c74d1aeeb675
MAC <user@MACs-MacBook-Air.local>
2022-06-03 11:55:11 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,81 @@ import Blog from './components/blogs/Blog'
 import CommingSoon from './components/commingSoon/CommingSoon'
 import Data  from './components/Data'
 import Owners from './components/owners/Owners'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
+import IMAGE from './assets/sample.png'
+
 
 const App = () => {
+  const blogs = [ 
+    {
+      id:1,
+      image:'./assets/sample.png',
+      date:'JANUARY 4, 2022 . FEATURED ARTICLE . AFFILATE',
+      heading:'12 Popup Use Cases To Increase Conversions',
+      body:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+    },
+    {
+      id:2,
+      image:{IMAGE},
+      date:'MAY 6, 2021 . FEATURED ARTICLE . AFFILATE',
+      heading:'2 Popup Use Cases To Increase Conversions',
+      body:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+    },
+    {
+      id:3,
+      image:{IMAGE},
+      date:'APRIL 23, 2021 . FEATURED ARTICLE . AFFILATE',
+      heading:'30 Popup Use Cases To Increase Conversions',
+      body:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+    },
+    {
+      id:4,
+      image:{IMAGE},
+      date:'MARCH 15, 2021 . FEATURED ARTICLE . AFFILATE',
+      heading:'929 Popup Use Cases To Increase Conversions',
+      body:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+    },
+    {
+      id:5,
+      image:{IMAGE},
+      date:'OCTOBER 8, 2021 . FEATURED ARTICLE . AFFILATE',
+      heading:'122 Popup Use Cases To Increase Conversions',
+      body:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+    },
+    {
+      id:6,
+      image:{IMAGE},
+      date:'FEBUARY 21, 2021 . FEATURED ARTICLE . AFFILATE',
+      heading:'12 Popup Use Cases To Increase Conversions',
+      body:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+    },
+  ]
   return (
-    <div>
-      {/* <Axios /> */}
-      {/* <Data/> */}
-      {/* <Owners/> */}
-      {/* <Header/> */}
-      {/* <CommingSoon/> */}
-      <Blog/>
-    </div>
+    <Router>
+    <>
+
+    <Switch>
+      <Route exact path='/'>
+      <CommingSoon/> 
+      </Route>
+    </Switch>
+
+
+
+    </>
+
+    </Router>
+
+
+      // {/* <Axios /> */}
+      // {/* <Data/> */}
+      // {/* <Owners/> */}
+      // {/* <Header/> */}
+      // {/* */}
+      // <Blog Blogs={blogs}/>
+    
   )
 }
 
--- src/components/blogs/Blog.jsx
+++ src/components/blogs/Blog.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import './blog.css'
 import Head from './head/Head'
+import IMAGE from '../../assets/sample.png'
 
-function Blog() {
+function Blog({Blogs}) {
   return (
     <>
     <Head/>
@@ -10,7 +11,55 @@ function Blog() {
         <div className="article-news">
           <div className="back-fade"></div>
           <div className="article-blur"></div>
+
+          <div className="article">
+            <div id='blog'>
+              <div className='line'></div>
+            <p>BLOG</p>
+            </div>
+            <h1>Articles and News</h1>
+
+          </div>
+
+
         </div>
+        <div className="body">
+          <div className="latest-search">
+            <div className="latest">
+              <h1>Latest From The Blog</h1>
+              <h4>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</h4>
+            </div>            
+              <div className="search">
+                <input type="text" placeholder='Search here' />
+              </div>
+          </div>
+
+          <div className="articles-container">
+
+            {
+              Blogs.map(blog => (
+                <article key={blog.id}>
+                  <img src={IMAGE} alt="" />
+                  <div className="date">{blog.date}</div>
+                  <div className="heading">{blog.heading}</div>
+                  <div className="article-body">{blog.body}</div>
+                  <div className="more">
+                  <div className="article-line"></div>
+                  <h5>READ MORE</h5>
+                  </div>
+                </article>)
+              )
+            }
+          
+
+
+          </div>
+
+          
+
+        </div>
+
+
       </div>
     </>
     
